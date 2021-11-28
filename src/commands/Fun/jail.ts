5d6acaeda674b4be296a73673e8fd90e52e1ac0b
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2021-11-28 21:48:40 +0530

    Fixed jail

--- src/commands/Fun/jail.ts
+++ src/commands/Fun/jail.ts
@@ -4,6 +4,11 @@ import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 import axios from 'axios'
+import { readFileSync, writeFileSync } from 'fs';
+import { tmpdir } from 'os';
+import request from '../../lib/request';
+// @ts-ignore
+import imgbbUploader from 'imgbb-uploader';
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
@@ -16,17 +21,22 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        const image = await (M.WAMessage?.message?.imageMessage
+        const image:any = await (M.WAMessage?.message?.imageMessage
             ? this.client.downloadMediaMessage(M.WAMessage)
             : M.quoted?.message?.message?.imageMessage
             ? this.client.downloadMediaMessage(M.quoted.message)
             : M.mentioned[0]
             ? this.client.getProfilePicture(M.mentioned[0])
             : this.client.getProfilePicture(M.quoted?.sender || M.sender.jid))
-
-            await axios.get(`https://some-random-api.ml/canvas/jail?avatar=${image}`)
+        const buff = await request.buffer(image)
+        const filename = `${tmpdir()}/pfp_${Math.random().toString(36)}`
+     const download = await  writeFileSync(`${filename}.jpg`,buff)
+     const ReadBuffer = await readFileSync(`${filename}.jpg`)
+     const result = await imgbbUploader('235ef89fb9a32fa804cddf4a7226d775', `${filename}.jpg`)
+    const pfp = result.url  
+            await request.buffer(`https://some-random-api.ml/canvas/jail?avatar=${pfp}`)
             .then((response)=>{
-                M.reply(response.data)
+              this.client.sendMessage(M.from,response,MessageType.image)
             }).catch((e)=>{
                 M.reply('sorry couldn\'t send the image')
             })
